50bf3ada0837a4728361fca6bdc9cd2f7774243b
Obyat Abdelrahman <rahman.obyat@gmail.com>
2021-01-30 21:43:07 -0500

    some implementation

--- js-basics/index.js
+++ js-basics/index.js
@@ -3,6 +3,12 @@ console.log("Test if reference is visible");
 console.log("Test if reference is visible2");
 //first logging
 
+let name="Obyat"
+console.log(name);
 
-
-
+var interestRate = 0.3;
+interestRate = 1;
+interestRate = undefined;
+name = null ;
+console.log(name);
+console.log(interestRate);
